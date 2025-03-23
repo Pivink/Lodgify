f98e70423dab3e96bd63e5c9420777f90fb3f19c
Pivink <161929786+Pivink@users.noreply.github.com>
2025-03-23 18:09:34 +0530

    Update razor_pay.js

--- razor_pay.js
+++ razor_pay.js
@@ -5,7 +5,7 @@ console.log("RAZORPAY_KEY_ID:", process.env.RAZORPAY_KEY_ID || "MISSING_KEY");
 console.log("RAZORPAY_KEY_SECRET:", process.env.RAZORPAY_KEY_SECRET || "MISSING_SECRET");
 
 const razorpay = new Razorpay({
-  key_id: process.env.RAZORPAY_KEY_ID,
+  key_id: "rzp_test_eThLGW3Me5FJvr",
   key_secret: process.env.RAZORPAY_KEY_SECRET
 });
 
