6609084afaa4dbd4e9be18b781ccef463dcc11a9
Pivink <161929786+Pivink@users.noreply.github.com>
2025-03-23 18:07:51 +0530

    Update razor_pay.js

--- razor_pay.js
+++ razor_pay.js
@@ -1,9 +1,12 @@
-const Razorpay = require('razorpay');
-require('dotenv').config();
+const Razorpay = require("razorpay");
+
+// Debug: Print keys before initializing Razorpay
+console.log("RAZORPAY_KEY_ID:", process.env.RAZORPAY_KEY_ID || "MISSING_KEY");
+console.log("RAZORPAY_KEY_SECRET:", process.env.RAZORPAY_KEY_SECRET || "MISSING_SECRET");
 
 const razorpay = new Razorpay({
-    key_id: process.env.RAZORPAY_KEY_ID,
-    key_secret: process.env.RAZORPAY_KEY_SECRET
+  key_id: process.env.RAZORPAY_KEY_ID,
+  key_secret: process.env.RAZORPAY_KEY_SECRET
 });
 
 module.exports = razorpay;
